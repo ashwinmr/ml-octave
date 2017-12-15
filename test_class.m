71a0cd425fb6f57aa4026c2027e8f9f0ed3f427d
Ashwin Rao <Ashwin.Rao@trw.com>
2017-12-15 14:35:45 -0500

    - Added set and get methods - Added functionality for higher order inputs

--- test_class.m
+++ test_class.m
@@ -1,31 +1,38 @@
 % Testing all functions in the class
 
 clc;
-clear all;
-close all;
+clear;
+close;
 
 addpath('Functions');
 addpath('Classes');
 
 % Load training data
-load('x');
-load('y');
+load('p');
+load('v');
 
 % Create an object of ML class
 ml = ML_C();
 
 % Set the x and y for the class
-ml.x = x;
-ml.y = y;
+x = [p,p.^2,p.^3];
+ml.Set_x(x);
+ml.Set_y(v);
 
 % Perform gradient descent
-alpha = 0.01;
+alpha = 0.1;
 n = 1500;
-ml.Gradient_Descent(alpha,n,1);
+ml.Gradient_Descent(1,alpha,n);
 
 % Predict
-y_predicted = ml.Predict(10);
+y_predicted = ml.Predict(x);
 
-
-
-%}
+% Plot
+plot(p,v,'*'); % Plot the training data
+hold all;
+plot(x(:,1),y_predicted); % Plot the prediction
+grid on;
+xlabel('x');
+ylabel('y');
+legend('Training Data','Prediction');
+hold off;
